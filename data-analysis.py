b7d55e990dacaa19d41deae88164191710f6c031
Léon van Velzen <leonvv@leonvv.leonvv.name>
2018-09-23 22:07:08 +0200

    Init

---
+++ data-analysis.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+import pyqtgraph as pg
+
+import interface
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+
+app = QApplication(sys.argv)
+
+window = QMainWindow() 
+
+generated_interface = interface.Ui_MainWindow()
+generated_interface.setupUi(window)
+generated_interface.plotWidget.plot(range(100), range(100))
+
+window.show()
+sys.exit(app.exec_())
+
---
+++ interface.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file './interface.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(678, 485)
+        MainWindow.setAccessibleName("")
+        MainWindow.setUnifiedTitleAndToolBarOnMac(False)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.dropdown = QtWidgets.QComboBox(self.centralwidget)
+        self.dropdown.setObjectName("dropdown")
+        self.dropdown.addItem("")
+        self.dropdown.addItem("")
+        self.dropdown.addItem("")
+        self.verticalLayout.addWidget(self.dropdown)
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout_2.addWidget(self.pushButton)
+        self.horizontalSlider = QtWidgets.QSlider(self.groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.horizontalSlider.sizePolicy().hasHeightForWidth())
+        self.horizontalSlider.setSizePolicy(sizePolicy)
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.verticalLayout_2.addWidget(self.horizontalSlider)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem)
+        self.verticalLayout.addWidget(self.groupBox)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.plotWidget = PlotWidget(self.centralwidget)
+        self.plotWidget.setObjectName("plotWidget")
+        self.horizontalLayout_4.addWidget(self.plotWidget)
+        self.horizontalLayout.addLayout(self.horizontalLayout_4)
+        self.verticalLayout_4.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem1 = QtWidgets.QSpacerItem(0, 0, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.horizontalLayout_2.addWidget(self.label)
+        self.verticalLayout_4.addLayout(self.horizontalLayout_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Physics Experiments Data Analysis"))
+        self.dropdown.setItemText(0, _translate("MainWindow", "Raw Data"))
+        self.dropdown.setItemText(1, _translate("MainWindow", "Frequency"))
+        self.dropdown.setItemText(2, _translate("MainWindow", "Filter"))
+        self.groupBox.setTitle(_translate("MainWindow", "GroupBox"))
+        self.pushButton.setText(_translate("MainWindow", "Some Button"))
+        self.label.setText(_translate("MainWindow", "© Tobias Gobel & Léon van Velzen"))
+
+from pyqtgraph import PlotWidget
